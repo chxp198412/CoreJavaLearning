3aff8f40dd3604095e2548f716f0d27ec8327e87
chxp198412 <andychen198412@gmail.com>
2019-12-03 21:40:35 -0500

    Sale Tickets

---
+++ src/com/andy/thread/ThreadMethodsTest.java
@@ -0,0 +1,53 @@
+package com.andy.thread;
+
+import static java.lang.Thread.sleep;
+
+class MyThread01 extends Thread{
+    public MyThread01(String name){
+        super(name);
+    }
+
+    @Override
+    public void run(){
+        for(int i = 0; i<=100; i++){
+            if(i%2 ==0){
+                //test sleep
+//                try {
+//                    sleep(1000);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+                System.out.println(Thread.currentThread().getName() + ":"+ getPriority() + i);
+            }
+            //测试yield方法
+//            if(i%20 == 0){
+//                yield();
+//            }
+        }
+    }
+}
+
+public class ThreadMethodsTest {
+    public static void main(String[] args){
+        MyThread01 t1 = new MyThread01("Thread 1");
+        //t1.setName("Thread 1");
+        t1.start();
+
+        //给主线程命名
+        Thread.currentThread().setName("Main Thread");
+        for(int i = 0; i<=100; i++){
+            if(i%2 !=0){
+                System.out.println(Thread.currentThread().getName() + ":"+ Thread.currentThread().getPriority() + i);
+            }
+
+            //测试join
+//            if(i == 20){
+//                try {
+//                    t1.join();
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+        }
+    }
+}
---
+++ src/com/andy/thread/TicketWindow.java
@@ -0,0 +1,10 @@
+package com.andy.thread;
+
+public class TicketWindow extends Thread{
+    @Override
+    public void run() {
+        while (true){
+            
+        }
+    }
+}
